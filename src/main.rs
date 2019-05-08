20bf37a19d7a239d0cbde223bd9e1bd4e5d127ed
Joshua Bussdieker <jbussdieker@gmail.com>
2019-05-08 14:04:52 -0700

    Refactor to use namespace

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,14 @@
-fn handle_instances(instance_list: std::vec::Vec<rusoto_ec2::Instance>) {
+use rusoto_core::Region::UsEast1;
+use rusoto_ec2::{Ec2, Ec2Client, Instance, Reservation, DescribeInstancesRequest, DescribeInstancesResult};
+use std::vec::Vec;
+
+fn handle_instances(instance_list: Vec<Instance>) {
     for instance in instance_list {
         println!("Instance ID: {}", instance.instance_id.expect("Missing instance ID"))
     }
 }
 
-fn handle_reservations(reservation_list: std::vec::Vec<rusoto_ec2::Reservation>) {
+fn handle_reservations(reservation_list: Vec<Reservation>) {
     for reservation in reservation_list {
         match reservation.instances {
             Some(instance_list) => {
@@ -15,7 +19,7 @@ fn handle_reservations(reservation_list: std::vec::Vec<rusoto_ec2::Reservation>)
     }
 }
 
-fn handle_result(result: rusoto_ec2::DescribeInstancesResult) {
+fn handle_result(result: DescribeInstancesResult) {
     match result.reservations {
         Some(reservation_list) => {
             handle_reservations(reservation_list)
@@ -25,10 +29,10 @@ fn handle_result(result: rusoto_ec2::DescribeInstancesResult) {
 }
 
 fn main() {
-    let client = rusoto_ec2::Ec2Client::new(rusoto_core::Region::UsEast1);
-    let describe_instances_request: rusoto_ec2::DescribeInstancesRequest = Default::default();
+    let client = Ec2Client::new(UsEast1);
+    let describe_instances_request: DescribeInstancesRequest = Default::default();
 
-    match rusoto_ec2::Ec2::describe_instances(&client, describe_instances_request).sync() {
+    match client.describe_instances(describe_instances_request).sync() {
         Ok(result) => {
             handle_result(result)
         },
